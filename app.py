3c99a122b105c88078b802550ec46c71513345e6
Cloga Chen <cloga0216@gmail.com>
2014-11-19 23:56:22 +0800

    xlsx

--- app.py
+++ app.py
@@ -48,124 +48,6 @@ dtype_mapping = {u'CURRENCY':'object', u'FLOAT' : float, u'INTEGER' : int, u'PER
 http = httplib2.Http(disable_ssl_certificate_validation=True)
 
 # url = 'https://www.googleapis.com/analytics/v3/data/ga?access_token=ya29.wwAl_oJqu37-d_ytQcGj-RtgANH1DAKshqai-bGFyTXlt8JrvJGZnjl-P2FXxFzF3_Z8eRoJtVkB9A&ids=ga:36050032&start-date=2014-01-01&end-date=2014-11-01&metrics=ga:pageviews,ga:sessions&dimensions=ga:source,ga:medium&max-results=10000'
-def get_data(args):
-    url = data_uri + '?' + args
-    content = json.loads(urllib2.urlopen(url).read())
-    return content
-    columns = [i['name'] for i in content['columnHeaders']]
-    dtypes = {i['name']:dtype_mapping.get(i['dataType'], None) for i in content['columnHeaders']}
-    pages = content['totalResults'] / 10000.0 + 1
-    rows = content['rows']
-    if pages >= 2:
-        for i in range(2, pages):
-                url0 = url + '&start-index=' + str(i * 10000) + str(1)
-                # print '解析数据,打开:\n' + url0
-                content0 = json.loads(urllib2.urlopen(url0).read())
-                rows += content0['rows']
-    df = pd.DataFrame(rows, columns=columns)
-    for c in df.columns:
-        df[c] = df[c].astype(dtypes[c])
-    return df
-
-
-def get_mcf_data(args):
-    url = mcf_uri + '?' + args
-    print '解析数据,打开:\n' + url
-    content = json.loads(urllib2.urlopen(url).read())
-    headers = [i['name'] for i in content['columnHeaders']]
-    rows = content['rows']
-    if content['totalResults'] <= 10000:
-        return headers, rows
-    else:
-        for i in range(2, content['totalResults'] / 10000 + 1):
-            url0 = url + '&start-index=' + str(i * 10000)
-            print '解析数据,打开:\n' + url0
-            content0 = json.loads(urllib2.urlopen(url0).read())
-            print len(rows)
-            rows += content0['rows']
-        return headers, rows
-
-
-def write_data(headers, rows, name='ga_data'):
-    content = ','.join(headers).encode(
-        'gbk', 'ignore') + '\n' + '\n'.join(rows)
-    with open('./' + name + '.csv', 'w') as f:
-        f.write(content)
-    print "Data has been writen!"
-
-
-def agg_dic(dic, key, value):  # 给定channel名，算channel的值
-    if dic.has_key(key):
-        ##已有channel
-        dic[key] = [float(dic[key][0]) + float(
-            value[0]), float(dic[key][1]) + float(value[1])]
-    else:
-        dic[key] = value  # 新channel
-
-
-def output_dic(file, dic):
-    for k in dic.keys():
-# print k,dic[k][0],dic[k][1]
-        file.write('%s,%s,%s\n' % (k, dic[k][0], dic[k][1]))
-    file.write('\n\n\n')
-
-
-def write_funnel_data(headers, rows, name='funnel_data'):
-    funnels_list = []
-    for i in rows:
-        funnels_list += [str(j['nodeValue'])
-                         for j in i[0]['conversionPathValue']]
-    covs = [str(i[1]['primitiveValue']) for i in rows]
-    vals = [str(i[2]['primitiveValue']) for i in rows]
-    lines = [funnels_list[n] + ',' + covs[n] + ',' + vals[n]
-             for n in range(len(funnels_list))]
-    content = ','.join(headers).encode(
-        'gbk', 'ignore') + '\n' + '\n'.join(lines)
-    with open('./' + name + '.csv', 'w') as f:
-        f.write(content)
-    print "Funnel Data has been writen!"
-
-
-def write_mcf_data(headers, rows, name='mcf_data'):
-    dic_f = {}  # first touch
-    dic_l = {}  # last touch
-    dic_ln = {}  # linear
-    funnels_list = []
-    for i in rows:
-        paths = [str(j['nodeValue']) for j in i[0]['conversionPathValue']]
-        covs = i[1]['primitiveValue']
-        vals = i[2]['primitiveValue']
-        if len(paths) == 1:
-            channel = paths
-            cov = [covs, vals]
-            agg_dic(dic_f, channel[0], cov)
-            agg_dic(dic_l, channel[0], cov)
-            agg_dic(dic_ln, channel[0], cov)
-        else:
-            # first touch
-            channel = paths[0]
-            cov = [covs, vals]
-            agg_dic(dic_f, channel, cov)
-            # last touch
-            channel = paths[-1]
-            agg_dic(dic_l, channel, cov)
-            # linear
-            channels_list = collections.Counter(paths).most_common()
-            for c in channels_list:
-                weight = c[1] / len(paths)
-                cov = [float(covs) * weight, float(vals) * weight]
-                agg_dic(dic_ln, c[0], cov)
-    with open(name + '.csv', 'w') as f:
-        f.write('-----first touch------\n')
-        f.write('channel,conversions,value\n')
-        output_dic(f, dic_f)
-        f.write('-----last touch------\n')
-        f.write('channel,conversions,value\n')
-        output_dic(f, dic_l)
-        f.write('-----linear------\n')
-        f.write('channel,conversions,value\n')
-        output_dic(f, dic_ln)
-        print "MCF Data has been writen!"
 
 #######################
 #### configuration ####
@@ -231,9 +113,23 @@ def query():
             '&metrics=' + str(metrics) +\
             '&dimensions=' + str(dimensions) +\
             '&max-results=' + '10000'   
-        # return 'ok'
-        df =  get_data(args)
-        return df
+        url = data_uri + '?' + args
+        return url
+        content = json.loads(urllib2.urlopen(url).read())
+        # return content
+        columns = [i['name'] for i in content['columnHeaders']]
+        dtypes = {i['name']:dtype_mapping.get(i['dataType'], None) for i in content['columnHeaders']}
+        pages = content['totalResults'] / 10000.0 + 1
+        rows = content['rows']
+        if pages >= 2:
+            for i in range(2, pages):
+                    url0 = url + '&start-index=' + str(i * 10000) + str(1)
+                    # print '解析数据,打开:\n' + url0
+                    content0 = json.loads(urllib2.urlopen(url0).read())
+                    rows += content0['rows']
+        df = pd.DataFrame(rows, columns=columns)
+        for c in df.columns:
+            df[c] = df[c].astype(dtypes[c])
         xlsx_file = tempfile.NamedTemporaryFile(dir=file_path, mode='w+b', suffix='.xlsx', delete=False)
         # df.to_excel('static/files/xlsx_file.xlsx', index=False)
         df.to_excel(xlsx_file.name, index=False)
