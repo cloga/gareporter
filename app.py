1e467fb599b3d4db1513e0b05cf82233a5c74e8b
Cloga Chen <cloga0216@gmail.com>
2014-12-01 22:38:37 +0800

    heroku test

--- app.py
+++ app.py
@@ -104,12 +104,12 @@ def query():
             '&dimensions=' + str(dimensions) +\
             '&max-results=' + '10000'   
         url = data_uri + '?' + args
-        return url
         content = json.loads(urllib2.urlopen(url).read())
         columns = [i['name'] for i in content['columnHeaders']]
         dtypes = {i['name']:dtype_mapping.get(i['dataType'], None) for i in content['columnHeaders']}
         pages = content['totalResults'] / 10000 + 1 if content['totalResults'] / 10000 != content['totalResults'] / 10000.0 else content['totalResults'] / 10000
         rows = content['rows']
+        return url
         if pages >= 2:
             for i in range(2, pages):
                     url0 = url + '&start-index=' + str(i * 10000) + str(1)
